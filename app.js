eb62badde0913eccd2b20590e63c8047ff8e2232
arindamad <32026868+arindamad@users.noreply.github.com>
2023-03-24 12:15:26 +0530

    Update in os

--- app.js
+++ app.js
@@ -6,6 +6,31 @@ const scale = window.devicePixelRatio; // Change to 1 on retina screens to see b
 console.log(ww, wh, scale)
 console.log(window.navigator);
 
+function getOS() {
+    var userAgent = window.navigator.userAgent,
+        platform = window.navigator?.userAgentData?.platform || window.navigator.platform,
+        macosPlatforms = ['Macintosh', 'MacIntel', 'MacPPC', 'Mac68K'],
+        windowsPlatforms = ['Win32', 'Win64', 'Windows', 'WinCE'],
+        iosPlatforms = ['iPhone', 'iPad', 'iPod'],
+        os = null;
+  
+    if (macosPlatforms.indexOf(platform) !== -1) {
+      os = 'Mac OS';
+    } else if (iosPlatforms.indexOf(platform) !== -1) {
+      os = 'iOS';
+    } else if (windowsPlatforms.indexOf(platform) !== -1) {
+      os = 'Windows';
+    } else if (/Android/.test(userAgent)) {
+      os = 'Android';
+    } else if (/Linux/.test(platform)) {
+      os = 'Linux';
+    }
+  
+    return os;
+  }
+  
+
+
 const outPut =  `<ul>
     <li>
         <b>Resulation : </b> ${ww} X ${wh}
@@ -14,7 +39,7 @@ const outPut =  `<ul>
         <b>Device Pixel Ratio : </b> ${scale}
     </li>
     <li>
-        <b>Platform : </b> ${navigator.platform}
+        <b>Operating System : </b> ${getOS()}
     </li>
 </ul>`
 
